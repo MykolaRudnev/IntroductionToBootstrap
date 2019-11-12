482575fffd97fcbabbaf48d435b9215d4456e306
MykolaRudnev <odessakolua@gmail.com>
2019-11-12 16:53:17 +0100

    First project using Bootstrap 4

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+ $('.header').height($(window).height());
+})
+
+$(document).ready(function(){
+    $("#menu").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 150);
+    });
+});
